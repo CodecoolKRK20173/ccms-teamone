6fa8d9cd9884b886578c16c9be53156b4e3409b4
Kemel3 <kemelZX@gmail.com>
2019-12-17 14:40:28 +0100

    Added connection to database

--- src/DataBase/Dao.java
+++ src/DataBase/Dao.java
@@ -1,9 +1,31 @@
 package DataBase;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Dao {
-    public static void connect() {
+    public static Connection connect() {
         Connection cnn = null;
+        try {
+            String username = "079yZXjtkg";
+            String passwordBase = "e4wKGw62Tl";
+            String url = "jdbc:mysql://remotemysql.com:3306/079yZXjtkg";
+            cnn = DriverManager.getConnection(url, username, passwordBase);
+            System.out.println("Connection to base has been established.");
+
+//            return cnn;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (cnn != null) {
+                    cnn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return cnn;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import DataBase.Dao;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+        Dao.connect();
     }
 }
