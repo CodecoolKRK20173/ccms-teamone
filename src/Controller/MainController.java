dac14634ef74793169167200600374ab7a6a2307
KaristonVeluraz <kg97@interia.pl>
2019-12-18 11:20:26 +0100

    Main controller add sqljcbd conn

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,9 +1,15 @@
 package Controller;
 
+import DataBase.SQLJDBC;
+
+import java.sql.SQLException;
+
 public class MainController {
 
-    public void run(){
+    public void run() throws SQLException {
         View_controller view_controller = new View_controller();
         view_controller.print("działa");
+        SQLJDBC sqljdbc = new SQLJDBC();
+
     }
 }
