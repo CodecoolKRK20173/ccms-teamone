859fe6db10fefb33d019b7b080108e47ff777bcb
KaristonVeluraz <kg97@interia.pl>
2019-12-18 14:13:22 +0100

    check login and password in database

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -2,18 +2,52 @@ package Controller;
 
 import DataBase.Dao;
 
+import javax.swing.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+
 
 public class LoginController  {
 
 
     public LoginController() throws SQLException {
        Dao dao = new Dao();
+        ViewController v = new ViewController();
 
-        Statement statement = dao.connect().createStatement();
-         ResultSet resultSet = statement.executeQuery("SELECT * from  Users");
+
+        try {
+            String UName = "Testo";
+            String password = "qwe";
+
+            String sql = "SELECT * FROM Users where Name='"+UName+"'and Password='"+password+"'";
+            Statement statement = dao.connect().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+
+
+            if(!resultSet.next()) {
+                v.write("wrong username or password");
+
+            }
+            else {
+                v.write("YOU ARE iN ");
+            }
+
+
+
+        } catch (SQLException sql) {
+            ViewController viewController = new ViewController();
+            v.write("sql exception");
+        }finally{
+            //closing ResultSet,PreparedStatement and Connection object
+
+
+        }
+
+
+
 
         System.out.println("loginController");
 
