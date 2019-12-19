aeea12f1aeeecf76763279de5c498cafb95f5699
KaristonVeluraz <kg97@interia.pl>
2019-12-19 11:07:00 +0100

    system exit to admin controller

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,14 +1,20 @@
 package Controller;
 
 import DataBase.Dao;
+import View.Menu;
+
+
+import javax.swing.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 
 
-public class LoginController {
-    private Dao dao = new Dao();
+public class LoginController  {
+    private  Dao dao = new Dao();
+    private Menu menu = new Menu();
+
 
     public LoginController() throws SQLException {
         ViewController vc = new ViewController();
@@ -22,22 +28,34 @@ public class LoginController {
                 ResultSet resultSQL = getResult(sql);
                 int accType = getAccountType(resultSQL);
                 vc.write("" + accType);
+
+                menu.adminMenu();
             }
         } catch (SQLException sql) {
             vc.error();
         } finally {
             dao.closeConnection();
         }
+
+
     }
 
+
     public int getAccountType(ResultSet resultSet) throws SQLException {
+
+
         int intType = 0;
         while (resultSet.next()) {
+
             String type = resultSet.getString("ID_Type");
+
             intType = Integer.parseInt(type);
+
+
         }
         return intType;
     }
+
 
     public ResultSet getResult (String sql) throws SQLException {
         return dao.connect().executeQuery(sql);
--- src/View/Menu.java
+++ src/View/Menu.java
@@ -5,16 +5,50 @@ import Controller.ViewController;
 public class Menu {
     private ViewController vc = new ViewController();
 
-    public String adminMenu() {
+    public void adminMenu() {
         System.out.println("(0) Exit");
         System.out.println("(1) Add new mentor");
         System.out.println("(2) List all mentors");
         System.out.println("(3) Remove mentor");
         System.out.println("(4) Edit mentor data");
         System.out.println("(5) List all students");
-        String choice = vc.input().nextLine();
-        return choice;
+        int choice = vc.input().nextInt();
+        adminChoose(choice);
     }
 
+    public void adminChoose(int number){
+        switch (number) {
+            case 0: {
+                System.exit(0);
+                break;
+            }
+            case 1: {
+                System.out.println("jeden");
+                break;
+            }
+            case 2: {
+                System.out.println("dwa");
+                break;
+            }
+            case 3: {
+                System.out.println("jtrzy");
+                break;
+            }
+            case 4: {
+                System.out.println("cztery");
+                break;
+            }
+            case 5: {
+                System.out.println("piec");
+                break;
+            }
+
+            default: {
+                adminMenu();
+                break;
+            }
+        }
+
+    }
 
 }
